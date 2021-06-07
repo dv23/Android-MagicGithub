dced1c2ad4df023ff0096e897d87f929335d5b95
Vincent <dubeaurepaire.v@gmail.com>
2021-06-07 11:53:39 +0200

    Nouveautés - Amelioration performance Repository (ApiService) : interface

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -3,38 +3,26 @@ package com.openclassrooms.magicgithub.repository;
 import com.openclassrooms.magicgithub.api.ApiService;
 import com.openclassrooms.magicgithub.model.User;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserRepository {
-    private final ApiService apiService;
-    private List<User> listeUsersApi;
-    //private List<User> listeUsersRepository;
+    private final ApiService RepositoryService;
 
     public UserRepository(ApiService apiService) {
-        this.apiService = apiService;
-        listeUsersApi=apiService.getUsers();
+        this.RepositoryService = apiService;
     }
 
     public List<User> getUsers() {
-        return listeUsersApi;
+        return RepositoryService.getUsers();
     }
 
     public void generateRandomUser() {
-        apiService.generateRandomUser();
-        listeUsersApi=apiService.getUsers();
+        //apiService.generateRandomUser();
+        RepositoryService.generateRandomUser();
+        RepositoryService.getUsers();
     }
 
     public void deleteUser(User user) {
-        apiService.deleteUser(user);
-    }
-
-    /**
-     * Récupération du dernier profil
-     * @return le dernier objet de la ocllection de profils, donc le plus récent
-     */
-    public int getLastUser() {
-        return apiService.getUsers().size()-1;
-        //return apiService.getUsers().size();
+        RepositoryService.deleteUser(user);
     }
 }
