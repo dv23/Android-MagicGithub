8d50baf389540c0804acd71f5020a8ca2929ca5c
Vincent <dubeaurepaire.v@gmail.com>
2021-06-11 16:36:44 +0200

    supprimer commentaires qui n'était pas là initialement

--- app/src/test/java/com/openclassrooms/magicgithub/UserRepositoryTest.java
+++ app/src/test/java/com/openclassrooms/magicgithub/UserRepositoryTest.java
@@ -31,7 +31,7 @@ import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInA
  */
 @RunWith(JUnit4.class)
 public class UserRepositoryTest {
-    
+
     private UserRepository userRepository;
     
     @Before
@@ -39,49 +39,21 @@ public class UserRepositoryTest {
         userRepository = Injection.createUserRepository();
     }
 
-    /** procédure à exécuter avant l'ensemble des tests,
-    @BeforeClass
-    public static void testPreCondition() throws Exception{
-        //assertNotNull(userRepository);
-        // procedure a executer avant l'ensemble des test;
-        //assertNotNull(userRepository.getUsers());
-    }*/
-
     @Test
     public void getUsersWithSuccess() {
         List<User> usersActual = userRepository.getUsers();
-        //List<User> usersActual = FakeApiService.getUsers();
-        System.out.println("Test getUsersWithSuccess :" );
-        /* Log.d("users repo","" + usersActual); */
         List<User> usersExpected = FAKE_USERS;
-        //Log.d("repository","test trace debug");
-        //assertThat(usersActual, hasItems(usersExpected));
         assertThat(usersExpected, containsInAnyOrder(usersActual.toArray()));
     }
 
     @Test
     public void generateRandomUserWithSuccess() {
         System.out.println("Test generateRandomUserWithSucces...: " );
-        //System.out.println(" generateRandomUserWithSuccess Test getUsers().size avt clear : " + userRepository.getUsers().size());
-
         userRepository.getUsers().clear();
-
-        //System.out.println(" generateRandomUserWithSuccess Test getUsers().size apres clear : " + userRepository.getUsers().size());
-
         userRepository.generateRandomUser();
-
-        //System.out.println(" generateRandomUserWithSuccess Test getUsers().size apres generatrandomuser : " + userRepository.getUsers().size());
-
         User user = userRepository.getUsers().get(0);
-
-        //System.out.println(" generateRandomUserWithSuccess Test getUsers().size(  " + userRepository.getUsers().size());
         assertEquals(1, userRepository.getUsers().size());
-
-        //System.out.println("test FAKE_USERS_RANDOM ..." + FAKE_USERS_RANDOM);
-        //System.out.println("generateRandomUserWithSuccess test user.getAvatarUrl ..." + user.getAvatarUrl());
-        //System.out.println("generateRandomUserWithSuccess test FAKE_SUER stream ..." + FAKE_USERS_RANDOM.stream().map(User::getAvatarUrl).collect(Collectors.toList()));
-
-        assertTrue(FAKE_USERS_RANDOM.stream().map(User::getId).collect(Collectors.toList()).contains(user.getId()));
+        assertTrue(FAKE_USERS_RANDOM.stream().map(User::getAvatarUrl).collect(Collectors.toList()).contains(user.getAvatarUrl()));
         assertTrue(FAKE_USERS_RANDOM.stream().map(User::getId).collect(Collectors.toList()).contains(user.getId()));
         assertTrue(FAKE_USERS_RANDOM.stream().map(User::getLogin).collect(Collectors.toList()).contains(user.getLogin()));
         assertFalse(FAKE_USERS.stream().map(User::getAvatarUrl).collect(Collectors.toList()).contains(user.getAvatarUrl()));
@@ -94,9 +66,6 @@ public class UserRepositoryTest {
         User userToDelete = userRepository.getUsers().get(0);
         System.out.println(" Test deleteUsersWithSuccess  : " );
         userRepository.deleteUser(userToDelete);
-        //System.out.println(" Test user size  " + TestUsers.size());
-        //System.out.println("Test deleteUsersWithSuccess : userRepository contains apr delete :" + userRepository.getUsers().contains(userToDelete));
         assertFalse(userRepository.getUsers().contains(userToDelete));
-        //assertTrue(userRepository.getUsers().contains(userToDelete));
     }
 }
